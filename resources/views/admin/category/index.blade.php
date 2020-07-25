b70f44b3b628443dbedb2476d138f6704d5ccba3
lies046 <skytree.skysky@gmail.com>
2020-07-25 20:42:38 +0900

    Add CategoryIndex

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -2,5 +2,24 @@
 @section('content')
 <h1>Category</h1>
 @foreach($category as $result)
-@endforeach
+
+  <table class="table table-striped table-hover table-sm table-bordered">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Category Name</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td>{{ $result->name }}</td>
+        <td><a href="" class="btn btn-primary btn-sm">Edit</a>
+            <a href="" class="btn btn-danger btn-sm">Delete</a>
+      </td>
+      </tr>
+    </tbody>
+  </table>
+@endforeach 
 @endsection
