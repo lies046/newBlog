3917298890c237ec7b6bc0f87d812276760a6954
lies046 <skytree.skysky@gmail.com>
2020-07-26 14:05:06 +0900

    Add Post

---
+++ resources/views/admin/post/index.blade.php
@@ -0,0 +1,39 @@
+@extends('template.home')
+@section('sub-title', 'Post')
+@section('content')
+
+@if(Session::has('success'))
+<div class="alert alert-success" role="alert">
+  {{ Session('success')}}
+</div>
+@endif
+  <a href="{{ route('category.create')}}" class="btn btn-info btn-sm">Add Post</a>
+  <br><br>
+  <table class="table table-striped table-hover table-sm table-bordered">
+    <thead>
+      <tr>
+        <th>No</th>
+        <th>Post</th>
+        <th>Category</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach($post as $result => $hasil)
+      <tr>
+        <td>{{ $result + $post->firstitem() }}</td>
+        <td>{{ $hasil->title }}</td>
+        <td>{{ $hasil->category->name }}</td>
+        <form action="{{ route('post.destroy', $hasil->id )}}" method="POST">
+          @csrf
+          @method('delete')
+          <td><a href="{{ route('post.edit', $hasil->id )}}" class="btn btn-primary btn-sm">Edit</a>
+          <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+        </form>
+      </td>
+      </tr>
+      @endforeach 
+    </tbody>
+  </table>
+  {{ $post->links()}}
+@endsection
