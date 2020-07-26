bfddd124801dbf3bd8aa2a00d6008bccba04aeb2
lies046 <skytree.skysky@gmail.com>
2020-07-26 11:33:54 +0900

    Add CategoryDelete

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -104,6 +104,9 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::findorfail($id);
+        $category->delete();
+
+        return redirect()->back()->with('success', 'Delete Category');
     }
 }
