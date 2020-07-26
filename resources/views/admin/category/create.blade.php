4a1f5c1778c53bd6604366392b62f815c62ca875
lies046 <skytree.skysky@gmail.com>
2020-07-26 10:26:33 +0900

    Add FlashMessage

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -2,6 +2,20 @@
 @section('sub-title', 'Add Category')
 @section('content')
 
+@if(count($errors)>0)
+  @foreach($errors->all() as $error)
+  <div class="alert alert-danger" role="alert">
+    {{ $error }}
+  </div>
+  @endforeach
+@endif
+
+@if(Session::has('success'))
+<div class="alert alert-success" role="alert">
+  {{ Session('success')}}
+</div>
+@endif
+
 <form action="{{ route('category.store')}}" method="POST">
 @csrf
 <div class="form-group">
