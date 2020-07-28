a71279b4ab2c23da53efe34604be342cbf33dfee
lies046 <skytree.skysky@gmail.com>
2020-07-28 11:00:50 +0900

    Modified route

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::resource('/category', 'CategoryController');
-Route::resource('/tag', 'TagController');
-Route::get('/post/delete', 'PostController@show_delete')->name('post.show_delete');
-Route::get('/post/restore/{id}', 'PostController@restore')->name('post.restore');
-Route::delete('/post/kill/{id}', 'PostController@kill')->name('post.kill');
-Route::resource('/post', 'PostController');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::resource('/category', 'CategoryController');
+    Route::resource('/tag', 'TagController');
+    Route::get('/post/delete', 'PostController@show_delete')->name('post.show_delete');
+    Route::get('/post/restore/{id}', 'PostController@restore')->name('post.restore');
+    Route::delete('/post/kill/{id}', 'PostController@kill')->name('post.kill');
+    Route::resource('/post', 'PostController');
+});
